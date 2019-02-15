ed7b15af05fa1351d7b940d36a895c0b10a37122
H3xept <h3xept@gmail.com>
2019-02-15 20:10:55 +0000

    I know bad practice

--- src/main.c
+++ src/main.c
@@ -1,9 +1,37 @@
 #include <ANSIsACurse/characters.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define MAX_CMD_LEN 500
+#define K_HOME_ENV "HOME"
+#define K_PATH_ENV "PATH"
+#define K_PS1_ENV "PS1"
+
+char* read_history() {
+	return "ls\ncd \"lolz\"";
+}
 
 int main(int argc, char const *argv[])
 {
-	c_color(C_COLOR_CODE_Red);
-	printf("HELLO WORLD\n");
+	char buffer[MAX_CMD_LEN];
+	char* home_directory = getenv(K_HOME_ENV);
+	char* env_path = getenv(K_PATH_ENV);
+	char* history = NULL;
+
+	if (chdir(home_directory) != 0) {
+		// DIE
+		printf("Could not change directory!");
+		exit(-1);
+	}
+
+	history = read_history();
+
+	while(1) {
+		printf("%s","Shellerino Diocanarino$ ");
+		getchar();
+	}
+
+	printf("Env: %s",home_directory);
 	return 0;
 }
