454ed37b8e85b3dae713c9a16c66f53a6b2fec13
Rory Brown <rorybrown660@gmail.com>
2019-02-24 19:52:15 +0000

    Added comments

--- src/execute_command.h
+++ src/execute_command.h
@@ -6,9 +6,16 @@
 #define SIGNATURE_ELF 0x7f454c46
 #define SIGNATURE_ELF_REVERSE 0x464c457f
 
-void execute_command(char* command);
-void execute_builtin(char* command, char** argv);
-void execute_shell_script(char* filename, char** argv);
-void execute_bin(char* filename, char** argv);
+// Executes given string as if it were a command. Currently handles its own argv parsing (move later)
+void execute_command(const char* command);
+
+// Executes a given command (with args) string as if it were a builtin. (Has a big switch of builtin functions)
+void execute_builtin(const char* command, const char** argv);
+
+// Executes given command + args as if it were a shell script. Wrapper for handle_script in script_handling.c
+void execute_shell_script(const char* filename, const char** argv);
+
+// Executes given command + args as if it were a binary file (program).
+void execute_bin(const char* filename, const char** argv);
 
 #endif
