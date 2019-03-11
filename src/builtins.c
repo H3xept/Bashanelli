d4463aa3e3931154cb19f505ea051c5624356918
Rory Brown <rorybrown660@gmail.com>
2019-03-11 18:51:17 +0000

    added builtin builtin

--- src/builtins.c
+++ src/builtins.c
@@ -12,10 +12,14 @@ static void builtin_cd(const char** argv);
 static void builtin_export(const char **argv);
 static void builtin_alias(const char **argv);
 static void builtin_unalias(const char **argv);
+static void builtin_builtin(const char **argv);
 
-static const char* const builtins_list[] = {"cd", "export", "alias", "unalias"};
+static const char* const builtins_list[] = {"cd", "export", "alias", "unalias", "builtin"};
 
 int builtin_id(const char* command){
+	if(!command){
+		return -1;
+	}
 	for(int i = 0; i < NUM_BUILTINS; i++){
 		if(!strcmp(command,*(builtins_list + i))){
 			return i;
@@ -41,6 +45,9 @@ void exec_builtin_id(const int id, const char** argv){
 			break;
 		case 3:
 			builtin_unalias(argv);
+			break;
+		case 4:
+			builtin_builtin(argv);
 			break;
 		default:
 			printf("Builtin id %d not found.\n", id);
@@ -131,4 +138,12 @@ if(!*(argv+1)){
 		p = *(argv+i);
 	}
 	return;
+}
+
+static void builtin_builtin(const char** argv){
+	if(!*(argv + 1)){
+		printf("builtin: usage: builtin [shell-builtin [args]]\n");
+		return;
+	}
+	exec_builtin_str(*(argv + 1), argv + 1);
 }
